12c3d27fb62110e9fc720c881e6a67df77539882
Yisus1932 <juncojesus53@gmail.com>
2025-04-06 20:59:40 -0500

    Add files via upload

--- pokimon/js/index.js
+++ pokimon/js/index.js
@@ -2,10 +2,15 @@ function Loguear() {
     let username = document.getElementById('username').value
     let password = document.getElementById('clave').value
 
-    if(username == 'Jesus' && password == '123456'){
+    let user = JSON.parse(localStorage.getItem("user"))
+
+    if (user == null) {
+        alert('No has iniciado sesion')
+        window.location.href = "../index.html"
+    } else if (user[2] == username && user[3] == password) {
+        localStorage.setItem("user", username);
+        window.location.href = "../vistas/Entrenador.html"
         alert('Usuario valido')
-    }else{
-        alert('usuario incorrecto')
-    }  
+    }
 
 }
